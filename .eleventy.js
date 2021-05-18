b049f296fe56c81ef0d3d070a8596242d73dca48
pgm-jandeschacht <jandesch@student.arteveldehs.be>
2021-05-18 17:53:17 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+module.exports = function (eleventyConfig) {
+
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addPassthroughCopy({
+    'src/main.css': 'assets/main.css',
+    'src/images': 'assets/images',
+    'src/js': 'assets/js'
+  });
+  return {
+    dir: {
+      input: 'views',
+      output: 'docs'
+    },
+    pathPrefix: '/portfolio/',
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
